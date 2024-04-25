ef814901e49e45aa31afcd5d7f34ef8f404f6615
SABA AZEEM <sabaazeem025@gmail.com>
2024-04-25 16:30:23 +0500

    Add pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home.tsx';
+import Signin from './pages/Signin.tsx';
+import About from './pages/About.tsx';
+import Signup from './pages/Signup.tsx';
+import Dashboard from './pages/Dashboard.tsx';
+import Projects from './pages/Projects.tsx';
 
 const App = () => {
   return (
-<div className='text-7xl'>      
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Signin.tsx
@@ -0,0 +1,11 @@
+
+
+const Signin = () => {
+  return (
+    <div className="text-9xl text-black">
+      Signin
+    </div>
+  )
+}
+
+export default Signin
