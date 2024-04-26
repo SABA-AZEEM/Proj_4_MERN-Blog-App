583cd756871ad566a910ddc210b44ab6c80e0f6d
SABA AZEEM <sabaazeem025@gmail.com>
2024-04-26 16:33:27 +0500

    add basic thing

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,21 +1,61 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons'; 
+import { faMagnifyingGlass,faSun,faMoon } from '@fortawesome/free-solid-svg-icons'; 
+import { useState } from 'react';
+import { Navbar, TextInput } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
+
+  const [modeIcon, setModeIcon] = useState(faSun);
+  const [searchTerm, setSearchTerm]= useState('');
+  const handleChangeIcon = ()=>{
+    modeIcon === faSun ? setModeIcon(faMoon) : setModeIcon(faSun) ;
+  }
+
+  const handleSubmit = ()=>{
+
+  }
+
   return (
-    <div>
-      <div>
-        <h1><span>Saba's</span>Blog</h1>
-      </div>
-      <div>
-        <input 
-            type="text"
-            placeholder='Search...'
+    <Navbar className='border-b-2'>
+      <Link 
+        to='/'
+        className='self-center text-sm sm:text-xl whitespace-nowrap font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-purple-900 via-purple-700 to-purple-400 rounded-lg text-white'>
+          Saba's
+        </span>
+        Blog
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <TextInput 
+          type='text'
+          placeholder='Search...'
+          rightIcon={faMagnifyingGlass}
+          className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e)=>setSearchTerm(e.target.value)}
         />
-        <FontAwesomeIcon icon={faHome} />
+      </form>
+      <div>
+        <nav>
+          <ul className='flex justify-between'>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/projects">Projects</a></li>
+          </ul>
+        </nav>
       </div>
-    </div>
+      <div>
+        <button onClick={handleChangeIcon}>
+        <FontAwesomeIcon icon={modeIcon} />
+        </button>
+      </div>
+      <div>
+        <a href="/signin">Sign In</a>
+      </div>
+    </Navbar>
   )
 }
 
